ef9fb03ac381e9304778889bf45342e715d0480d
Satya-S3 <satyasahu644@gmail.com>
2023-09-02 22:06:08 +0530

    first

---
+++ app.js
@@ -0,0 +1,48 @@
+const url='https://api.openweathermap.org/data/2.5/weather?appid=7ea12571dac848e26b09be93e8d48f5b&units=metric&q=';
+
+const search=document.querySelector('.search input');
+const searchBtn=document.querySelector('.search-btn');
+const weatherImg=document.querySelector('.weather-img');
+
+async function fetchAPI(city){
+      const response=await fetch(url+city);
+      if(response.status==404){
+            weatherImg.src='images/error.png';
+            document.querySelector('.city').children[1].innerHTML="INVALID CITY";
+            document.querySelector('.temp').innerHTML='';
+            document.querySelector('.humidity').innerHTML='';
+            document.querySelector('.wind').innerHTML=''; 
+      }
+      else{
+            const data=await response.json();
+            console.log(data);
+            document.querySelector('.city').children[1].innerHTML=data.name;
+            document.querySelector('.temp').innerHTML=Math.round(data.main.temp)+" °C";
+            document.querySelector('.humidity').innerHTML=data.main.humidity+" %";
+            document.querySelector('.wind').innerHTML=data.wind.speed+" km/hr";
+            if(data.weather[0].main=='Clouds'){
+                  weatherImg.src='images/clouds.png';
+            }
+            else if(data.weather[0].main=='Clear'){
+                  weatherImg.src='images/clear.png';
+            }
+            else if(data.weather[0].main=='Rain'){
+                  weatherImg.src='images/rain.png';
+            }
+            else if(data.weather[0].main=='Drizzle'){
+                  weatherImg.src='images/drizzle.png';
+            }
+            else if(data.weather[0].main=='Mist'){
+                  weatherImg.src='images/mist.png';
+            }
+            else if(data.weather[0].main=='Haze'){
+                  weatherImg.src='images/haze.png';
+            }
+      }
+}
+
+searchBtn.addEventListener('click',()=>{
+      fetchAPI(search.value);
+      search.value="";
+})
+
